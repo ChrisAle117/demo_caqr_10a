58b3866b617d9f8b945aa6d73c8d8f458462eb11
“ChrisAle117” <20223tn105@utez.edu.mx>
2025-10-17 19:41:04 -0600

    [Commit]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -34,3 +35,33 @@ function App() {
 }
 
 export default App
+=======
+import TestController from "./modules/test/test.controller";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a href="" className="navbar-brand">ADJ-Demo</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="" className="nav-link active">Home</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container mt-5">
+        <h2>Inicios</h2>
+        <hr />
+        <button onClick={() => TestController.callToAPI()} className="btn btn-success">Llamar API</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
+>>>>>>> 4e9ad9d ([commit])
---
+++ server/src/main/java/adj/demo/server/AdjApplication.java
@@ -0,0 +1,13 @@
+package adj.demo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdjApplication.class, args);
+    }
+
+}
