303202a52096a7660c386894fca25b3f3bc7a9f9
“ChrisAle117” <20223tn105@utez.edu.mx>
2025-10-21 17:45:05 -0600

    commit 14

--- server/src/main/java/adj/demo/server/utils/DBConnection.java
+++ server/src/main/java/adj/demo/server/utils/DBConnection.java
@@ -28,26 +28,9 @@ public class DBConnection {
     public DataSource getDBConnection() {
         DriverManagerDataSource source = new DriverManagerDataSource();
         source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + name;
-        int retries = 10;
-        int waitMs = 3000;
-        for (int i = 0; i < retries; i++) {
-            try {
-                source.setUrl(url);
-                source.setUsername(user);
-                source.setPassword(pass);
-                // Try to get a connection
-                source.getConnection().close();
-                return source;
-            } catch (Exception e) {
-                if (i == retries - 1) {
-                    throw new RuntimeException("No se pudo conectar a la base de datos tras varios intentos", e);
-                }
-                try {
-                    Thread.sleep(waitMs);
-                } catch (InterruptedException ignored) {}
-            }
-        }
-        throw new RuntimeException("No se pudo conectar a la base de datos");
+        source.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
+        source.setUsername(user);
+        source.setPassword(pass);
+        return source;
     }
 }
